648f12b463868c6509f166894920cb13a0a69a8f
Xushnora <xushnoranishonova@gmail.com>
2022-04-14 21:36:52 -0700

    changed

--- js/main.js
+++ js/main.js
@@ -30,9 +30,9 @@ for(let i = 1; i<=8; i++){
 
 arr.forEach((e,i)=>{
 let li = document.createElement('li')
-li.classList.add('a'+e.x , 'b'+e.y )
-li.className = 'chesItem'
-    if((e.x + e.y) % 2 ==0) {
+// li.classList.add('chesItem');
+li.classList.add('a'+e.x , 'b'+e.y, 'chesItem')
+    if((e.x + e.y) % 2 == 0) {
         li.style.background = `#f0d9b5`;
         li.style.background = `url(../imgs/wood.jpg)`;
     } else {
@@ -47,31 +47,85 @@ elUl.appendChild(li)
 let chesItems = document.querySelectorAll('.chesItem');
 
 chesItems.forEach((e, i) => {
-    e.addEventListener('mouseover', ()=>{
-        let acl = e.classList[0];
-        let bcl = e.classList[1];
-        let alilar = document.querySelectorAll(`.${acl}`)
-        let blilar = document.querySelectorAll(`.${bcl}`)
-
-            corX.textContent = `x = ${e.textContent[0]}`;
-            corY.textContent = `y = ${e.textContent[3]}`;
-            x = e.textContent[0];
-            y = e.textContent[3];
-
-        e.classList.add('red');
-        console.log(e.classList[0]);
+    e.addEventListener('mouseover', () =>{
+        corX.textContent = `x = ${e.textContent[0]}`;
+        corY.textContent = `y = ${e.textContent[3]}`;
+        x = e.textContent[0];
+        y = e.textContent[3];
     })
-    e.addEventListener('mouseout', ()=>{
-        let acl = e.classList[0]
-        let bcl = e.classList[1]
-        let alilar = document.querySelectorAll(`.${acl}`)
-        let blilar = document.querySelectorAll(`.${bcl}`)
-
-        e.classList.remove('red');
-        console.log(e.classList[0]);
+    tora.addEventListener('click', (els) =>{
+        // els.currentTarget.style.background = '#fff';
+        e.addEventListener('mouseover', ()=>{
+            let acl = e.classList[0];
+            let bcl = e.classList[1];
+            let alilar = document.querySelectorAll(`.${acl}`)
+            let blilar = document.querySelectorAll(`.${bcl}`)
+    
+            alilar.forEach(j=>{
+                j.classList.add('green');
+            })
+            blilar.forEach(j=>{
+                j.classList.add('green');
+            })
+    
+            e.classList.add('red');
+            console.log(e.classList[0]);
+        })
+        e.addEventListener('mouseout', ()=>{
+            let acl = e.classList[0]
+            let bcl = e.classList[1]
+            console.log(e.classList[0]);
+            console.log(e.classList[1]);
+            let alilar = document.querySelectorAll(`.${acl}`)
+            let blilar = document.querySelectorAll(`.${bcl}`)
+    
+            alilar.forEach(j=>{
+                j.classList.remove('green');
+            })
+            blilar.forEach(j=>{
+                j.classList.remove('green');
+            })
+    
+            e.classList.remove('red');
+            console.log(e.classList[0]);
+        })
     })
+   
 })
 
+chesItems.forEach((e, i) => {
+    e.addEventListener('mouseover', () =>{
+        corX.textContent = `x = ${e.textContent[0]}`;
+        corY.textContent = `y = ${e.textContent[3]}`;
+        x = e.textContent[0];
+        y = e.textContent[3];
+    })
+    vazir.addEventListener('click', (elv) =>{
+        // e.currentTarget.style.background = 'green'
+        e.addEventListener('mouseover', () =>{
+            let axOld = x - 1;
+            let ayOld = y - 1;
+            let axAfter = x + 1;
+            let ayAfter = y + 1;
+            console.log(x , y, axOld, ayOld, axAfter, ayAfter);
+            // console.log(y);
+
+        })
+
+    })
+   
+})
+
+
 ot.addEventListener('click', (e)=>{
-    e.currentTarget.style.background = 'gold'
+    // e.currentTarget.style.background = 'gold'
+})
+
+shoh.addEventListener('click', (e) =>{
+    // e.currentTarget.style.background = 'green'
+})
+
+
+vazir.addEventListener('click', (e) =>{
+    // e.currentTarget.style.background = 'green'
 })
